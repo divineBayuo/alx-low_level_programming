f8ab9206566e84d6161025c39d0b13501386d08f
Divine Bayuo <divinewb123@gmail.com>
2022-06-06 14:48:55 -0700

    Print string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-/*this cfile demonstrates
- *the use of puts
- */
+/*this cfile demonstrates the use of puts*/
 
-int main (void)
+int main(void)
 {
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(mystr);
-
 	return (0);
 }

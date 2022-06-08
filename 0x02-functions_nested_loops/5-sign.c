f1f4b1e2311ea429e8ca04c5cf9466c195a12d10
Divine Bayuo <divinewb123@gmail.com>
2022-06-08 16:52:06 -0700

    Print sign of character

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar(43);
+		_putchar('43');
 	}
 	else if (n < 0)
 	{
 		return (-1);
-		_putchar(45);
+		_putchar('45');
 	}
 	else
 	{
 		return (0);
-		_putchar(48);
+		_putchar('48');
 	}
 	return (0);
 }

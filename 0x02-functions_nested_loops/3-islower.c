0a1cbf5c43991c86c901a89a06dc0afafd3b98d7
Divine Bayuo <divinewb123@gmail.com>
2022-06-08 14:49:05 -0700

    Print islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function _islower
+ * Return (0) success
+ */
+
+int _islower(int)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		return (1);
+	}
+
+	return (0);
+}

5443f70969fad49617267fd42b2a5689c403adeb
Divine Bayuo <divinewb123@gmail.com>
2022-06-08 16:07:42 -0700

    heck for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - _isalpha checks for alphabetic character
+ *
+ * @c: takes type integer(int c)
+ *
+ * Return: (0) success
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 && c >= 97 && c <= 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}

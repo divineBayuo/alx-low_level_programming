400319110dfbdc7b2fdaadc406997a0cda2c008f
Divine Bayuo <divinewb123@gmail.com>
2022-06-08 09:16:47 -0700

    Print alphabet x10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,19 +3,16 @@
  * main - print alphabets with putchar
  * Return (0) success
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	print_alphabet();
+	for (i = 0; i < 27; i++)
 	{
-		for (i = 0; i < 27; i++)
-		{
-			_putchar(alphabet[i]);
-		}
-		_putchar('\n');
+		_putchar(alphabet[i]);
 	}
+	_putchar('\n');
 
 	return (0);
 }

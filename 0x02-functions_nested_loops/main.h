7deebe2c26dd9c3c7048ddda47737806faba0ddc
Divine Bayuo <divinewb123@gmail.com>
2022-06-07 17:02:57 -0700

    Create file that prints with putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,4 @@
-#include <unistd.h>
+/*This is the header file main.h
+ * contains declarations for putchar */
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print 
- *
- * Return: On success 1.
- * On error, -1 is returned, and error number is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);

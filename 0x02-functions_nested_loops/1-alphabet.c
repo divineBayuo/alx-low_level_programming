d78043a37b7c16ddc808442dc73e9d58a1eb69c7
Divine Bayuo <divinewb123@gmail.com>
2022-06-08 09:35:04 -0700

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * main - print alphabets with putchar
+ * main - print_alphabet
  * Return (0) success
  */
 void print_alphabet(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 27; i++)
+	for (i = 97; i < 124; i++)
 	{
-		_putchar(alphabet[i]);
+		_putchar(i);
 	}
 	_putchar('\n');
 }

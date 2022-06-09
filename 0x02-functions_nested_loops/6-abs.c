865d92f6fa72ef28a2ae1a949d84ce694ac02d9a
Divine Bayuo <divinewb123@gmail.com>
2022-06-08 22:55:39 -0700

    Find abs value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _abs(int) - function _abs(int) finds the abs value of an integer
+ * _abs(int) - function _abs(int) finds the absolute value of an integer
  *
  * @n: takes type integer(int n)
  *

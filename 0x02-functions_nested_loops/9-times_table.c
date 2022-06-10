97a1e3513107d7e4bc2774827cebfc4e05c2e8e5
Divine Bayuo <divinewb123@gmail.com>
2022-06-10 06:21:39 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,18 +18,20 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			mul = a * b;
-			_putchar(44);
-			_putchar(32);
 
 			if (mul <= 9)
 			{
 				_putchar(32);
 				_putchar(mul + 48);
+				_putchar(44);
+				_putchar(32);
 			}
 			else
 			{
 				_putchar((mul / 10) + 48);
 				_putchar((mul % 10) + 48);
+				_putchar(44);
+				_putchar(32);
 			}
 		}
 		_putchar('\n');

d39dadad674ef477605eed7e1197fc395e184a95
Divine Bayuo <divinewb123@gmail.com>
2022-06-10 08:38:32 -0700

    Print 9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@
 void times_table(void)
 {
 	int a, b, mul;
+
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
@@ -17,6 +18,7 @@ void times_table(void)
 		for (b = 1; b <= 9; b++)
 		{
 			mul = a * b;
+
 			_putchar(44);
 			_putchar(32);
 

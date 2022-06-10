90ebb4d570449aff4e2f81ac546ed99927b59a57
Divine Bayuo <divinewb123@gmail.com>
2022-06-10 15:36:58 -0700

    Print numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,29 +9,29 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n > 98)
 	{
-		while (n >= 97)
+		while (n > 98)
 		{
 			printf("%d", n);
 			printf(",");
 			printf(" ");
 			n--;
 		}
-		printf("%d", n);
+		printf("98");
 		printf(" ");
 	}
 
-	else if (n <= 97)
+	else if (n < 98)
 	{
-		while (n <= 97)
+		while (n < 98)
 		{
 			printf("%d", n);
 			printf(",");
 			printf(" ");
 			n++;
 		}
-		printf("%d", n);
+		printf("98");
 		printf(" ");
 	}
 

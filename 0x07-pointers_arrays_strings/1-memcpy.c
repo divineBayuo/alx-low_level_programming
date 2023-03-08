52906066abc45ff30553c0f853f1340f65ab931f
Divine Bayuo <divinewb123@gmail.com>
2023-03-08 20:52:10 +0000

    Edit file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _memcpy - copies memory area,
+ * _memcpy - copies memory area
  *
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: bytes filled.
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: bytes filled
  * 
- * Return: the pointer to dest.
+ * Return: the pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)

30dc8ef8a44ad74f4d3c6ab030553b5ec230cf03
Divine Bayuo <divinewb123@gmail.com>
2023-03-09 12:31:27 +0000

    Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string
+ * _print_rev_recursion - function that prints a string
  *
  * @s: input string
  *
  * Return: no return
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }

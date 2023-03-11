c8bb3133bb75b248bb62c89e2c62391e505ff4ec
Divine Bayuo <divinewb123@gmail.com>
2023-03-11 21:05:15 +0000

    Reorganise files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - printing a-z and A-Z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alps = 'a';
+	char alpC = 'A';
+
+	while (alps < 'z' + 1)
+	{
+		putchar(alps);
+		alps++;
+	}
+
+	while (alpC < 'Z' + 1)
+	{
+		putchar(alpC);
+		alpC++;
+	}
+	putchar('\n');
+
+	return (0);
+}

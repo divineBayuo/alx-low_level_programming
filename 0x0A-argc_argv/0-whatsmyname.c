a4a77830e256deb5933512e48341faeaa9da7dcf
Divine Bayuo <divinewb123@gmail.com>
2023-03-14 17:03:22 +0000

    Print prog name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: int count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: Success Always (0)
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}

b439febd9fb69e6edfa1be32b1ca8e14ec7a3084
Divine Bayuo <divinewb123@gmail.com>
2023-03-14 17:39:17 +0000

    Print num of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: int count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: Success Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

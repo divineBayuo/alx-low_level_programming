87f778f33c24d2e43283cf904ac73a28b560c4cd
Divine Bayuo <divinewb123@gmail.com>
2023-03-22 00:16:02 +0000

    Create header file to perform sun

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * FILE: 4-sum.h
+ * DESC: Header file that defines the function-like macro
+ *	SUM(x, y) which computes the sum of x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif

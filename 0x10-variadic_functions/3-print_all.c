6ee7358db3c13f8ecb0db99f25c4a2ae2c874713
Divine Bayuo <divinewb123@gmail.com>
2023-03-25 05:42:29 +0000

    Print all strings

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,9 +9,9 @@ void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 /**
- * print_char - Prints char.
- * @arg: A list of arguments pointing to the character
- *	to be printed.
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
  */
 void print_char(va_list arg)
 {
@@ -23,8 +23,8 @@ void print_char(va_list arg)
 
 /**
  * print_int - Prints an int.
- * @arg: A list of arguments pointing to the integer
- *	to be printed.
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
  */
 void print_int(va_list arg)
 {
@@ -36,8 +36,8 @@ void print_int(va_list arg)
 
 /**
  * print_float - Prints a float.
- * @arg: A list of arguments pointing to the float
- *	to be printed.
+ * @arg: A list of arguments pointing to
+ *       the float to be printed.
  */
 void print_float(va_list arg)
 {
@@ -49,21 +49,22 @@ void print_float(va_list arg)
 
 /**
  * print_string - Prints a string.
- * @arg: A list of arguments pointing to the string
- *	to be printed.
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
  */
 void print_string(va_list arg)
 {
-	char *s;
+	char *str;
 
-	s = va_arg(arg, char *);
+	str = va_arg(arg, char *);
 
-	if (s == NULL)
+	if (str == NULL)
 	{
 		printf("(nil)");
 		return;
 	}
-	printf("%s", s);
+
+	printf("%s", str);
 }
 
 /**
@@ -71,13 +72,14 @@ void print_string(va_list arg)
  * @format: A string of characters representing the argument types.
  * @...: A variable number of arguments to be printed.
  *
- * Description: Any argument not of type char, int, float, or char *
- *	is ignored. If a string is NULL, print (nil).
+ * Description: Any argument not of type char, int, float,
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int a = 0, b = 0;
+	int i = 0, j = 0;
 	char *separator = "";
 	printer_t funcs[] = {
 		{"c", print_char},
@@ -88,21 +90,23 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format && (*(format + a)))
+	while (format && (*(format + i)))
 	{
-		b = 0;
+		j = 0;
 
-		while (b < 4 && (*(format + a) != *(funcs[b].symbol)))
-			b++
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
 
-		if (b < 4)
+		if (j < 4)
 		{
 			printf("%s", separator);
-			funcs[b].print(args);
+			funcs[j].print(args);
 			separator = ", ";
 		}
-		a++;
+
+		i++;
 	}
+
 	printf("\n");
 
 	va_end(args);

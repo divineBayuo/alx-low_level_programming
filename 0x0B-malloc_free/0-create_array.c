692f31bf335ac082240b383d49485999f12cbb08
Divine Bayuo <divinewb123@gmail.com>
2023-03-27 14:59:33 +0000

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,13 +4,13 @@
 
 /**
  * create_array - function that creates an array of chars,
- * 		and initializes it with a specific char
+ *		and initializes it with a specific char
  *
  * @size: size of the array
  * @c: content of the array
  *
  * Return: NULL if size = 0
-	else pointer to the array
+ *	else pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
@@ -26,7 +26,7 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL);
 
-	for (a = 0; a < size, a++)
+	for (a = 0; a < size; a++)
 		array[a] = c;
 
 	return (array);

5b366bc4ca4f0f63b7dd3edd30d587e8ed69373a
Divine Bayuo <divinewb123@gmail.com>
2023-06-08 16:02:04 +0000

    _strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	if (*s != '\0')
+	if (*s == '\0')
+		return (0);
+	else
 	{
-		len++;
-		_strlen_recursion(s + 1);
-		return (len);
+		return(1 + _strlen_recursion(s + 1));
 	}
 }

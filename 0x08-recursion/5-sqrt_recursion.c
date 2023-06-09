8c576bbfd897d4199f3b27dc69aa9aaae56fcf05
Divine Bayuo <divinewb123@gmail.com>
2023-06-09 09:42:19 +0000

    sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -31,6 +31,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0)
 		return (0);
-	if (n > 0)
-		return (power_operation(n, 2)); 
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2)); 
 }

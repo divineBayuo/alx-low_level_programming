3699f1d48d7b3932afc23580d5b4809c2553e440
Divine Bayuo <divinewb123@gmail.com>
2023-06-11 02:17:30 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: number of cmdline arguments
+ * @argv: array of cmdline arguments
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

146ac422091cbd1f896eecd9f812113ce57d8822
Divine Bayuo <divinewb123@gmail.com>
2023-06-13 17:59:03 +0000

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	gr = (int *)malloc(sizeof(int *));
+	gr = (int *)malloc(sizeof(int *) * height);
 	if (gr == NULL)
 	{
 		free(gr);

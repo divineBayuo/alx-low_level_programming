8181911e3eea11c66c14bbe46275d7b0567375fa
Divine Bayuo <divinewb123@gmail.com>
2023-06-13 16:58:08 +0000

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *new;
 	unsigned int a, b, c, end;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (a = 0; s1[a] != '\0'; a++)
 		;

c6c1e6db4e16781411223c4c1108e9e198402707
Divine Bayuo <divinewb123@gmail.com>
2023-06-15 17:08:05 +0000

    Create main

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,12 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size of memory
- * Return: pointer to allocated memory
+ * Return: pointer to allocated memory, else exit
+ * with status value of 98
  */
 
 void *malloc_checked(unsigned int b)

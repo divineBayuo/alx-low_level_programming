b92947e266d927421bfdd5c5d29dc05a76806174
Divine Bayuo <divinewb123@gmail.com>
2023-06-19 23:44:15 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure called dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: this structure creates a
+ * dog with name, age and owner details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+/**
+ * dog - Typedef for struct dog
+ */
+typedef struct dog dog;
+
+#endif /* DOG_H */

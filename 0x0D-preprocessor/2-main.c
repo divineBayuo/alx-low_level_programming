bfc90face26ef69417fb581463ef8fa4fcbf24af
Divine Bayuo <divinewb123@gmail.com>
2023-06-19 23:18:53 +0000

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the name of the file the
+ * program is compiled from
+ *
+ * Return: Always Success (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

33f9a19dd8bbc1e9d76dad9e5668b828dad79206
Divine Bayuo <divinewb123@gmail.com>
2023-06-20 00:26:44 +0000

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -17,9 +15,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(*d).name = name;
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
-	return ();
 }

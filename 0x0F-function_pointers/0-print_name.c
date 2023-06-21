8771aee01f738264821dd9d4a602b0db9216ece4
Divine Bayuo <divinewb123@gmail.com>
2023-06-21 22:55:28 +0000

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function pointer to print function
+ *
+ * Return: no return
+ */
+
+void print_name(char *name, void (*f)(char *)
+{
+	if (name && f)
+		f(name);
+}

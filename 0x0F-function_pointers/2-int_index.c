a5047bed8f98952fe4d77700b70dde0fdefa5cf7
Divine Bayuo <divinewb123@gmail.com>
2023-06-21 23:47:12 +0000

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array of elements
+ * @size: size of the array
+ * @cmp: function pointer to function
+ *
+ * Return: no return
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (a = 0; a < size; a++)
+			if (cmp(array[a]))
+				return (i);
+	}
+	return (-1);
+}

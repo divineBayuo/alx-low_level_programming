8fba084a4831d66c4ec971f8437613eac01eaa5f
Divine Bayuo <divinewb123@gmail.com>
2023-06-22 23:03:48 +0000

    Create 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int a;
-	int sum = 0;
-
 	va_list args;
+	unsigned int a,	int sum = 0;
 
 	va_start(args, n);
+
+	if (n == 0)
+		return (0);
 
 	for (a = 0; a < n; a++)
 		sum += va_arg(args, int);

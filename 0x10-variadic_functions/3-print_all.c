93a9226bb7d21d4c46c12e0ccb6877433cc1e068
Divine Bayuo <divinewb123@gmail.com>
2023-06-23 17:27:37 +0000

    Create 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,7 @@ void print_all(const char * const format, ...)
 	va_list args;
 
 	index = 0;
-	va_start(args, format)
+	va_start(args, format);
 
 	while (format != NULL && *(format + index) != '\0')
 	{

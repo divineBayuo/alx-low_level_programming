2173b62408819b604b1cc8ab5d95846b1edf3a12
Divine Bayuo <divinewb123@gmail.com>
2023-07-03 11:35:12 +0000

    Create 1-list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h: pointer to the elements in a linked list
+ * Return: the number of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num_elements;
+
+	num_elements = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			num_elements = 1;
+		h = h->next;
+		num_elements++;
+	}
+	return (num_elements);
+}

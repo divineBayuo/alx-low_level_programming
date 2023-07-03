257386f2271ccb2c968f55cc361b1ddfe5a67263
Divine Bayuo <divinewb123@gmail.com>
2023-07-03 15:45:29 +0000

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to the list
+ * Return: no return
+ */
+
+void free_list(list_t *head)
+{
+	list_t copy;
+
+	while ((copy = head) != NULL)
+	{
+		head = head->next;
+		free(copy->str);
+		free(copy);
+	}
+}

1eaa0c4b6eb98b00ca75e780cb168d57c2cbdc44
Divine Bayuo <divinewb123@gmail.com>
2023-07-03 02:24:17 +0000

    Update header file

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct listing - singly linked list
+ * @str: string created with malloc()
+ * @len: length of string
+ * @next: pointer to the next node
+ *
+ * Description: structure for singly-linked list node
+ */
+typedef struct listing
+{
+	char *str;
+	unsigned int len;
+	struct listing *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
-#endif /* MAIN_H &*/
+#endif /* MAIN_H */

4f9ae21d82a872d32e8107e7a8199b8f0b23ebf0
Divine Bayuo <divinewb123@gmail.com>
2023-07-03 16:32:03 +0000

    Create 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	line db "Hello, Holberton!", 10, 0	;10 - ASCII code for newline, 0 - null
+
+section .text
+	extern printf
+
+global _start
+
+_start:
+	sub rsp, 8		;stack align
+	mov edi, line		;string address load
+	xor eax, eax		;clear register
+	call printf		;call printf func
+
+	xor edi, edi		;exit status 0
+	mov eax, 60		;system call num for exit
+	syscall			;call OS

4addf458d9a487a92c3f7e9dd4cda3b884026820
Divine Bayuo <divinewb123@gmail.com>
2023-07-04 19:17:33 +0000

    Create header file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - function that prints all the
+ * elements of a listint_t
+ * @h: pointer to the list
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint - singly linked list
+ * @n: integer input for list
+ * @next: pointer to the next node
+ *
+ * Description: structure for singly-linked list node
+ */
+typedef struct listint
+{
+	unsigned int n;
+	struct listint *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */

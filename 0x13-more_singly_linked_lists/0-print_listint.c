7f7671eb2c5e8536aee54474ea210019871b8517
Divine Bayuo <divinewb123@gmail.com>
2023-07-05 13:17:40 +0000

    Create 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,5 +12,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	
+	size_t num_elements;
+
+	num_elements = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_elements++;
+	}
+	return (num_elements);
 }

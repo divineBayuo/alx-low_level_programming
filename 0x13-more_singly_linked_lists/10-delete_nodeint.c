05f29a10f66a74ae32f9901e2fdaac655a6f6bf9
Divine Bayuo <divinewb123@gmail.com>
2023-07-13 17:28:56 +0000

    Update file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 	}
 
-	if (before == NULL || (before->next == NULL && index != 0)
+	if (before == NULL || (before->next == NULL && index != 0))
 		return (-1);
 
 	after = before->next;

8d8b9c032a065d3eeac8a52cb8f4a48adf8b2414
Divine Bayuo <divinewb123@gmail.com>
2023-07-13 10:27:27 +0000

    Update file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return ("");
+		text_content = "";
 
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
 		;
